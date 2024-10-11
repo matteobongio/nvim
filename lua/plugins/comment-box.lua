3731b9922c0454cbd5868a0fbd6c17d91fda0ca0
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-11 10:33:43 +0200

    comment box

--- lua/plugins/comment-box.lua
+++ lua/plugins/comment-box.lua
@@ -1,3 +1,7 @@
 return {
-  "LudoPinelli/comment-box.nvim"
+  "LudoPinelli/comment-box.nvim",
+  lazy = true,
+  keys = {
+    { "<leader>Cb", "<cmd>CBccbox<CR>", desc = "[C]omment [B]ox Title", mode = {"n", "v"} }
+  }
 }
