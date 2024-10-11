fe1330c5663dccdf6ea5c57ae8fe0581c93e86bd
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2024-10-11 15:28:44 +0200

    transparent

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,6 +1,6 @@
 return {
   "xiyaowong/transparent.nvim",
-  -- config = function ()
-  --   require('transparent').clear_prefix('lualine')
-  -- end
+  config = function ()
+    require('transparent').clear_prefix('GitSigns')
+  end
 }
