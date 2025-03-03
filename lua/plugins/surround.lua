bbd5085d11834fd938fd09556ac56ccaa99a9282
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2025-03-03 14:06:17 +0100

    format surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,18 +1,22 @@
-return { 'echasnovski/mini.surround', version = '*', opts = {
-  custom_surroundings = {
+return {
+  'echasnovski/mini.surround',
+  version = '*',
+  opts = {
+    custom_surroundings = {
       ['('] = { output = { left = '(', right = ')' } },
       [')'] = { output = { left = '( ', right = ' )' } },
-  },
-  mappings = {
-    add = 'gsa', -- Add surrounding in Normal and Visual modes
-    delete = 'gsd', -- Delete surrounding
-    find = 'gsf', -- Find surrounding (to the right)
-    find_left = 'gsF', -- Find surrounding (to the left)
-    highlight = 'gsh', -- Highlight surrounding
-    replace = 'gsr', -- Replace surrounding
-    update_n_lines = 'gsn', -- Update `n_lines`
+    },
+    mappings = {
+      add = 'gsa',          -- Add surrounding in Normal and Visual modes
+      delete = 'gsd',       -- Delete surrounding
+      find = 'gsf',         -- Find surrounding (to the right)
+      find_left = 'gsF',    -- Find surrounding (to the left)
+      highlight = 'gsh',    -- Highlight surrounding
+      replace = 'gsr',      -- Replace surrounding
+      update_n_lines = 'gsn', -- Update `n_lines`
 
-    suffix_last = 'l', -- Suffix to search with "prev" method
-    suffix_next = 'n', -- Suffix to search with "next" method
-  },
-} }
+      suffix_last = 'l',    -- Suffix to search with "prev" method
+      suffix_next = 'n',    -- Suffix to search with "next" method
+    },
+  }
+}
