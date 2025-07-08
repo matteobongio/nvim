744b5e878a8445901b28f496b5d747056d177c3f
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2025-07-08 22:34:50 +0200

    nerd font

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ are first encountering a few different constructs in your nvim config.
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 
 -- Put anything you want to happen only in Neovide here
 if vim.g.neovide then
